1170945c5fdbb052b047fd39282b5e1a6b00d4c9
Ameerul <ameerulzakiy@gmail.com>
2023-08-07 08:52:32 +0800

    0708

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // optional: mute and pause the video on click
-document.querySelector('.video-container video').addEventListener('click', function() {
+document.querySelector('.video-container video').addEventListener('click', function () {
   if (this.paused) {
     this.play();
   } else {
     this.pause();
   }
+});
+
+new WOW().init();
+
+
+/* AUTHOR LINK */
+$(document).ready(function() {
+  $('.about-me-img').hover(function() {
+    $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+  }, function() {
+    $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+  });
 });
